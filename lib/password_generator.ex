1a0071a6a2a48572e5b40a9323c00bd657b33d81
Lourens Kok <lourenskok@gmail.com>
2024-05-22 14:52:19 +0200

    password gen function

--- lib/password_generator.ex
+++ lib/password_generator.ex
@@ -1,18 +1,9 @@
 defmodule PasswordGenerator do
-  @moduledoc """
-  Documentation for `PasswordGenerator`.
-  """
+  @charset "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+"
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> PasswordGenerator.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def generate_password(length) when length > 0 do
+    1..length
+    |> Enum.map(fn _ -> Enum.random(@charset) end)
+    |> Enum.join()
   end
 end
